9f613455019c178844ab83995e27ff5ffce2e555
thenoseman <frank.schumacher.privat@gmail.com>
2024-07-12 16:53:07 +0200

    Update tabs.query

--- src/browser.js
+++ src/browser.js
@@ -3,13 +3,13 @@ export function getBrowser() {
 }
 
 export async function getCurrentTabInfo() {
-  const tabs = await getBrowser().tabs.query({active: true, currentWindow: true});
+  const tabs = await getBrowser().tabs.query({ active: true, lastFocusedWindow: true });
   const tab = tabs && tabs[0];
 
   return {
     id: tab ? tab.id : "",
     url: tab ? tab.url : "",
-    title: tab ? tab.title : ""
+    title: tab ? tab.title : "",
   };
 }
 
@@ -20,7 +20,7 @@ function useChromeStorage() {
 export function getStorageItem(key) {
   if (useChromeStorage()) {
     const result = chrome.storage.local.get([key]);
-    return result.then(data => data[key])
+    return result.then((data) => data[key]);
   } else {
     return Promise.resolve(localStorage.getItem(key));
   }
@@ -28,7 +28,7 @@ export function getStorageItem(key) {
 
 export function setStorageItem(key, value) {
   if (useChromeStorage()) {
-    return chrome.storage.local.set({[key]: value});
+    return chrome.storage.local.set({ [key]: value });
   } else {
     localStorage.setItem(key, value);
     return Promise.resolve();
