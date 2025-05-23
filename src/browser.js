6b966e76794eb8505fbcb6bebbc10cf9cc95d4cf
Dirk Holtwick <dirk.holtwick@gmail.com>
2025-05-23 18:25:46 +0200

    Fixes #29 Add link to open linkding web site

--- src/browser.js
+++ src/browser.js
@@ -1,40 +1,40 @@
 export function getBrowser() {
-  return typeof browser !== "undefined" ? browser : chrome;
+  return typeof browser !== "undefined" ? browser : chrome
 }
 
 export async function getCurrentTabInfo() {
   const tabs = await getBrowser().tabs.query({
     active: true,
     currentWindow: true,
-  });
-  const tab = tabs && tabs[0];
+  })
+  const tab = tabs && tabs[0]
 
   return {
     id: tab ? tab.id : "",
     url: tab ? tab.url : "",
     title: tab ? tab.title : "",
-  };
+  }
 }
 
 function isFirefox() {
-  return typeof browser !== "undefined";
+  return typeof browser !== "undefined"
 }
 
 function useChromeScripting() {
-  return typeof chrome !== "undefined" && !!chrome.scripting;
+  return typeof chrome !== "undefined" && !!chrome.scripting
 }
 
 export async function getBrowserMetadata() {
   const tabs = await getBrowser().tabs.query({
     active: true,
     currentWindow: true,
-  });
-  const tab = tabs && tabs[0];
+  })
+  const tab = tabs && tabs[0]
 
   const errorHandler = (error) => {
-    console.error("Failed to load browser metadata", error);
-    return { title: "", description: "" };
-  };
+    console.error("Failed to load browser metadata", error)
+    return { title: "", description: "" }
+  }
 
   if (useChromeScripting()) {
     function getMetadata() {
@@ -43,7 +43,7 @@ export async function getBrowserMetadata() {
         document
           .querySelector('meta[property="og:title"]')
           ?.getAttribute("content") ||
-        "";
+        ""
       const description =
         document
           .querySelector('meta[name="description"]')
@@ -51,8 +51,8 @@ export async function getBrowserMetadata() {
         document
           .querySelector('meta[property="og:description"]')
           ?.getAttribute("content") ||
-        "";
-      return { title, description };
+        ""
+      return { title, description }
     }
 
     return getBrowser()
@@ -61,7 +61,7 @@ export async function getBrowserMetadata() {
         func: getMetadata,
       })
       .then((result) => result[0].result)
-      .catch(errorHandler);
+      .catch(errorHandler)
   } else {
     const code = `
       (function () {
@@ -81,12 +81,12 @@ export async function getBrowserMetadata() {
         "";
         return { title, description };
       })();
-    `;
+    `
 
     return getBrowser()
       .tabs.executeScript(tab.id, { code })
       .then((result) => result[0])
-      .catch(errorHandler);
+      .catch(errorHandler)
   }
 }
 
@@ -95,66 +95,72 @@ export function getStorage() {
     typeof browser !== "undefined" &&
     typeof browser.storage !== "undefined"
   ) {
-    return browser.storage.local;
+    return browser.storage.local
   } else if (
     typeof chrome !== "undefined" &&
     typeof chrome.storage !== "undefined"
   ) {
-    return chrome.storage.local;
+    return chrome.storage.local
   } else {
-    throw new Error("Storage API not found.");
+    throw new Error("Storage API not found.")
   }
 }
 
 export async function getStorageItem(key) {
-  const storage = getStorage();
-  const results = await storage.get([key]);
-  let data = results[key];
+  const storage = getStorage()
+  const results = await storage.get([key])
+  let data = results[key]
 
   if (!data) {
     // Try lookup in local storage as fallback, which was used for storing
     // settings in Firefox before switching to storage API
     try {
-      data = localStorage.getItem(key);
+      data = localStorage.getItem(key)
     } catch (e) {
       // Ignore
     }
   }
 
-  return data;
+  return data
 }
 
 export function setStorageItem(key, value) {
-  const storage = getStorage();
-  return storage.set({ [key]: value });
+  const storage = getStorage()
+  return storage.set({ [key]: value })
 }
 
 export function openOptions() {
-  getBrowser().runtime.openOptionsPage();
-  window.close();
+  getBrowser().runtime.openOptionsPage()
+  window.close()
+}
+
+export function openLinks(url) {
+  const browserInstance = getBrowser()
+  browserInstance.tabs.create({ url })
+  window.close()
 }
 
 export function showBadge(tabId) {
-  const browser = getBrowser();
-  const action = browser.browserAction || browser.action;
-  action.setBadgeText({ text: "★", tabId: tabId });
-  action.setBadgeTextColor({ color: "#FFE234", tabId: tabId });
+  const browser = getBrowser()
+  const action = browser.browserAction || browser.action
+  action.setBadgeText({ text: "★", tabId: tabId })
+  action.setBadgeTextColor({ color: "#FFE234", tabId: tabId })
   action.setBadgeBackgroundColor({
     color: "rgba(100,100,100,1)",
     tabId: tabId,
-  });
+  })
 }
 
 export function removeBadge(tabId) {
-  const browser = getBrowser();
-  const action = browser.browserAction || browser.action;
-  action.setBadgeText({ text: "", tabId: tabId });
+  const browser = getBrowser()
+  const action = browser.browserAction || browser.action
+  action.setBadgeText({ text: "", tabId: tabId })
 }
 
 export function runSinglefile() {
-  const browser = getBrowser();
+  const browser = getBrowser()
   const extensionId = isFirefox()
     ? "{531906d3-e22f-4a6c-a102-8057b88a1a63}"
-    : "mpiodijhokgodhhofbcjdecpffjipkle";
-  browser.runtime.sendMessage(extensionId, "save-page");
+    : "mpiodijhokgodhhofbcjdecpffjipkle"
+  browser.runtime.sendMessage(extensionId, "save-page")
 }
--- src/form.svelte
+++ src/form.svelte
@@ -1,107 +1,122 @@
 <script>
-  import TagAutocomplete from './TagAutocomplete.svelte'
-  import {getBrowserMetadata, getCurrentTabInfo, openOptions, showBadge, runSinglefile} from "./browser";
-  import {loadServerMetadata, clearCachedServerMetadata} from "./cache";
-  import {getProfile, updateProfile} from "./profile";
-  import {getConfiguration} from "./configuration";
+  import TagAutocomplete from "./TagAutocomplete.svelte"
+  import {
+    getBrowserMetadata,
+    getCurrentTabInfo,
+    openOptions,
+    openLinks,
+    showBadge,
+    runSinglefile,
+  } from "./browser"
+  import { loadServerMetadata, clearCachedServerMetadata } from "./cache"
+  import { getProfile, updateProfile } from "./profile"
+  import { getConfiguration } from "./configuration"
 
-  export let api;
-  export let configuration;
+  export let api
+  export let configuration
 
-  let url = "";
-  let title = "";
-  let titlePlaceholder = "";
-  let descriptionPlaceholder = "";
-  let description = "";
-  let notes = "";
-  let tags = "";
-  let autoTags = "";
-  let unread = false;
-  let shared = false;
-  let saveState = "";
-  let errorMessage = "";
+  let url = ""
+  let title = ""
+  let titlePlaceholder = ""
+  let descriptionPlaceholder = ""
+  let description = ""
+  let notes = ""
+  let tags = ""
+  let autoTags = ""
+  let unread = false
+  let shared = false
+  let saveState = ""
+  let errorMessage = ""
   let availableTagNames = []
-  let bookmarkExists = false;
-  let editNotes = false;
-  let profile = null;
-  let tabInfo = null;
-  let extensionConfiguration = null;
-  let loading = false;
+  let bookmarkExists = false
+  let editNotes = false
+  let profile = null
+  let tabInfo = null
+  let extensionConfiguration = null
+  let loading = false
 
   $: {
     if (api && configuration) {
-      init();
+      init()
     }
   }
 
   async function init() {
     // First get cached user profile to quickly show something, then update it
     // in the background
-    profile = await getProfile();
-    updateProfile().then(updatedProfile => {
-      profile = updatedProfile;
-    });
+    profile = await getProfile()
+    updateProfile().then((updatedProfile) => {
+      profile = updatedProfile
+    })
 
     // Load available tags in the background
-    tags = configuration.default_tags;
-    api.getTags().catch(() => []).then(tags => {
-      availableTagNames = tags.map(tag => tag.name)
-    });
+    tags = configuration.default_tags
+    api
+      .getTags()
+      .catch(() => [])
+      .then((tags) => {
+        availableTagNames = tags.map((tag) => tag.name)
+      })
 
     // Initialize bookmark form
-    await initForm();
-    extensionConfiguration = await getConfiguration();
+    await initForm()
+    extensionConfiguration = await getConfiguration()
   }
 
   async function initForm() {
-    tabInfo = await getCurrentTabInfo();
-    url = tabInfo.url;
+    tabInfo = await getCurrentTabInfo()
+    url = tabInfo.url
 
-    loading = true;
+    loading = true
     const [serverMetadata, browserMetadata] = await Promise.all([
       loadServerMetadata(url),
       getBrowserMetadata(url),
-    ]);
-    loading = false;
+    ])
+    loading = false
 
     if (configuration.useBrowserMetadata) {
-      title = browserMetadata.title;
-      description = browserMetadata.description;
+      title = browserMetadata.title
+      description = browserMetadata.description
     } else {
-      title = serverMetadata.metadata.title;
-      description = serverMetadata.metadata.description;
+      title = serverMetadata.metadata.title
+      description = serverMetadata.metadata.description
     }
 
-    shared = configuration.shareSelected;
-    unread = configuration.unreadSelected;
+    shared = configuration.shareSelected
+    unread = configuration.unreadSelected
 
     // If the bookmark already exists, prefill the form with the existing bookmark
     if (!serverMetadata) {
-      return;
+      return
     }
 
-    const existingBookmark = serverMetadata.bookmark;
+    const existingBookmark = serverMetadata.bookmark
     if (existingBookmark) {
-      bookmarkExists = true;
-      title = existingBookmark.title;
-      tags = existingBookmark.tag_names ? existingBookmark.tag_names.join(" ") : "";
-      description = existingBookmark.description;
-      notes = existingBookmark.notes;
-      unread = existingBookmark.unread;
-      shared = existingBookmark.shared;
-      autoTags = "";
+      bookmarkExists = true
+      title = existingBookmark.title
+      tags = existingBookmark.tag_names
+        ? existingBookmark.tag_names.join(" ")
+        : ""
+      description = existingBookmark.description
+      notes = existingBookmark.notes
+      unread = existingBookmark.unread
+      shared = existingBookmark.shared
+      autoTags = ""
     } else {
       // Only show auto tags for new bookmarks
       // Auto tags are only supported since v1.31.0, so we need to check if they are available
-      const autoTagsList = serverMetadata.auto_tags;
+      const autoTagsList = serverMetadata.auto_tags
       if (autoTagsList) {
-        autoTags = autoTagsList.join(" ");
+        autoTags = autoTagsList.join(" ")
       }
     }
   }
 
   async function handleSubmit() {
-    const tagNames = tags.split(" ").map(tag => tag.trim()).filter(tag => !!tag);
+    const tagNames = tags
+      .split(" ")
+      .map((tag) => tag.trim())
+      .filter((tag) => !!tag)
     const bookmark = {
       url,
       title: title || "",
@@ -110,71 +125,93 @@
       tag_names: tagNames,
       unread,
       shared,
-    };
+    }
 
     try {
-      saveState = "loading";
+      saveState = "loading"
       await api.saveBookmark(bookmark, {
-        disable_html_snapshot: extensionConfiguration?.runSinglefile
-      });
-      await clearCachedServerMetadata();
-      saveState = "success";
+        disable_html_snapshot: extensionConfiguration?.runSinglefile,
+      })
+      await clearCachedServerMetadata()
+      saveState = "success"
       // Show star badge on the tab to indicate that it's now bookmarked
       // but only if precaching is enabled, since the badge will never
       // show when browsing without precaching
       if (extensionConfiguration?.precacheEnabled) {
-        showBadge(tabInfo.id);
+        showBadge(tabInfo.id)
       }
       // Close popup window after saving the bookmark, if configured
-      if (extensionConfiguration?.closeAddBookmarkWindowOnSave === true && extensionConfiguration?.closeAddBookmarkWindowOnSaveMs >= 0) {
+      if (
+        extensionConfiguration?.closeAddBookmarkWindowOnSave === true &&
+        extensionConfiguration?.closeAddBookmarkWindowOnSaveMs >= 0
+      ) {
         window.setTimeout(() => {
           window.close()
-        }, extensionConfiguration?.closeAddBookmarkWindowOnSaveMs);
+        }, extensionConfiguration?.closeAddBookmarkWindowOnSaveMs)
       }
       // Run singlefile, if configured
       if (!bookmarkExists && extensionConfiguration?.runSinglefile) {
-        runSinglefile();
+        runSinglefile()
       }
     } catch (e) {
-      saveState = "error";
-      errorMessage = e.toString();
-      console.error(errorMessage);
+      saveState = "error"
+      errorMessage = e.toString()
+      console.error(errorMessage)
     }
   }
 
   function handleOptions() {
-    openOptions();
+    openOptions()
+  }
+
+  function handleLinks() {
+    openLinks(configuration.baseUrl)
   }
 
   function toggleNotes() {
-    editNotes = !editNotes;
+    editNotes = !editNotes
   }
-
 </script>
+
 <div class="title-row">
   <h6>{bookmarkExists ? "Edit Bookmark" : "Add bookmark"}</h6>
-  <a href="#" on:click|preventDefault={handleOptions}>Options</a>
+  <div>
+    <a href="#" on:click|preventDefault={handleLinks}>Links</a>
+    &nbsp;|&nbsp;
+    <a href="#" on:click|preventDefault={handleOptions}>Options</a>
+  </div>
 </div>
 <form class="form" on:submit|preventDefault={handleSubmit}>
   <div class="form-group">
     <label class="form-label" for="input-url">URL</label>
     <div class="has-icon-right">
-      <input class="form-input" type="text" id="input-url" placeholder="URL"
-             bind:value={url}>
+      <input
+        class="form-input"
+        type="text"
+        id="input-url"
+        placeholder="URL"
+        bind:value={url}
+      />
       {#if loading}
         <i class="form-icon loading"></i>
       {/if}
     </div>
     {#if bookmarkExists}
       <div class="form-input-hint text-warning">
-        This URL is already bookmarked. The form has been prefilled from the existing bookmark, and saving the form will
-        update the existing bookmark.
+        This URL is already bookmarked. The form has been prefilled from the
+        existing bookmark, and saving the form will update the existing
+        bookmark.
       </div>
     {/if}
   </div>
   <div class="form-group">
     <label class="form-label" for="input-tags">Tags</label>
-    <TagAutocomplete id="input-tags" name="tags" bind:value={tags} tags={availableTagNames}/>
+    <TagAutocomplete
+      id="input-tags"
+      name="tags"
+      bind:value={tags}
+      tags={availableTagNames}
+    />
     {#if autoTags}
       <div class="form-input-hint text-success">
         Auto tags: {autoTags}
@@ -183,62 +220,91 @@
   </div>
   <div class="form-group">
     <label class="form-label" for="input-title">Title</label>
-    <input class="form-input" type="text" id="input-title"
-           bind:value={title} placeholder={titlePlaceholder}>
+    <input
+      class="form-input"
+      type="text"
+      id="input-title"
+      bind:value={title}
+      placeholder={titlePlaceholder}
+    />
   </div>
   <div class="form-group">
     {#if !editNotes}
       <div class="form-label-row">
         <label class="form-label" for="input-description">Description</label>
-        <button type="button" class="btn btn-link" on:click|preventDefault={toggleNotes}>Edit notes</button>
+        <button
+          type="button"
+          class="btn btn-link"
+          on:click|preventDefault={toggleNotes}>Edit notes</button
+        >
       </div>
-      <textarea class="form-input" id="input-description"
-                bind:value={description}
-                placeholder={descriptionPlaceholder}></textarea>
+      <textarea
+        class="form-input"
+        id="input-description"
+        bind:value={description}
+        placeholder={descriptionPlaceholder}
+      ></textarea>
     {/if}
     {#if editNotes}
       <div class="form-label-row">
         <label class="form-label" for="input-notes">Notes</label>
-        <button type="button" class="btn btn-link" on:click|preventDefault={toggleNotes}>Edit description
+        <button
+          type="button"
+          class="btn btn-link"
+          on:click|preventDefault={toggleNotes}
+          >Edit description
         </button>
       </div>
-      <textarea class="form-input" id="input-notes" rows="5"
-                bind:value={notes}></textarea>
+      <textarea class="form-input" id="input-notes" rows="5" bind:value={notes}
+      ></textarea>
     {/if}
   </div>
   <div class="form-group d-flex">
     <label class="form-checkbox">
-      <input type="checkbox" bind:checked={unread}>
+      <input type="checkbox" bind:checked={unread} />
       <i class="form-icon"></i>
       <span>Mark as unread</span>
     </label>
-    {#if profile?.enable_sharing }
+    {#if profile?.enable_sharing}
       <label class="form-checkbox ml-4">
-        <input type="checkbox" bind:checked={shared}>
+        <input type="checkbox" bind:checked={shared} />
         <i class="form-icon"></i>
         <span>Share</span>
       </label>
     {/if}
   </div>
   <div class="footer">
-    {#if saveState === 'success'}
+    {#if saveState === "success"}
       <div class="result-row text-success">
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
-             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-          <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-          <path d="M5 12l5 5l10 -10"/>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M5 12l5 5l10 -10" />
         </svg>
         <span>Bookmark saved</span>
       </div>
     {/if}
-    {#if saveState === 'error'}
+    {#if saveState === "error"}
       <div class="result-row text-error">
         Error saving bookmark: {errorMessage}
       </div>
     {/if}
-    {#if saveState !== 'success'}
+    {#if saveState !== "success"}
       <div class="button-row">
-        <button type="submit" class="btn btn-primary btn-wide" class:loading={saveState === 'loading'}>Save</button>
+        <button
+          type="submit"
+          class="btn btn-primary btn-wide"
+          class:loading={saveState === "loading"}>Save</button
+        >
       </div>
     {/if}
   </div>
